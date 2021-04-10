d6e16c07d04cddfbd7e6a20759297527d16b5fcc
Rafael Santos <raf535@outlook.com>
2021-04-10 17:04:57 -0300

    Refatorando questao do Pudean

--- mata37/lista2/E.cpp
+++ mata37/lista2/E.cpp
@@ -1,42 +1,57 @@
 #include <iostream>
+#include <climits>
 
 #define endl '\n'
 #define ll long long
-#define desync ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0)
+#define desync                    \
+	ios_base::sync_with_stdio(0); \
+	cout.tie(0);                  \
+	cin.tie(0)
 
 using namespace std;
 
-int main(){
+int main()
+{
 	desync;
 
-	ll C, O, L, X, qnt_C, qnt_O, qnt_L, qnt_X, qnt, ans, h, m, s;
+	ll C, O, L, X, qnt_C, qnt_O, qnt_L, qnt_X, ans, h, m, s, min = INT_MAX;
 
 	cin >> C >> O >> L >> X;
 
-	qnt_C = C/4;
-	qnt_O = O/8;
-	qnt_L = L/2;
-	qnt_X = X/1;
+	qnt_C = C / 4;
+	qnt_O = O / 8;
+	qnt_L = L / 2;
+	qnt_X = X / 1;
 
-	if(qnt_C <= qnt_O && qnt_C <= qnt_L && qnt_C <= qnt_X){
-		qnt = qnt_C;
-	}else if(qnt_O <= qnt_C && qnt_O <= qnt_L && qnt_O <= qnt_X){
-		qnt = qnt_O;
-	}else if(qnt_L <= qnt_C && qnt_L <= qnt_O && qnt_L <= qnt_X){
-		qnt = qnt_L;
-	}else if(qnt_X <= qnt_O && qnt_X <= qnt_L && qnt_X <= qnt_C){
-		qnt = qnt_X;
+	if (qnt_C < min)
+	{
+		min = qnt_C;
 	}
 
-	ans = qnt * 1259;
-	
-	h = ans/3600;
+	if (qnt_O < min)
+	{
+		min = qnt_O;
+	}
 
-	m = ((ans%3600)/60);
+	if (qnt_L < min)
+	{
+		min = qnt_L;
+	}
 
-	s = (ans - (m*60) - (h*3600));
+	if (qnt_X < min)
+	{
+		min = qnt_X;
+	}
 
-	cout << h << " h " << m << " m " << s <<  " s" << endl;
+	ans = min * 1259;
+
+	h = ans / 3600;
+
+	m = ((ans % 3600) / 60);
+
+	s = (ans - (m * 60) - (h * 3600));
+
+	cout << h << " h " << m << " m " << s << " s" << endl;
 
 	return 0;
 }
